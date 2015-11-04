703a3be6c6972551950aed8f21ad21f53e299e08
Robin <robin-wagner@web.de>
2015-11-04 21:28:47 +0100

    Find and update listname

--- assignment/assignment.rb
+++ assignment/assignment.rb
@@ -52,15 +52,10 @@ class Assignment
   #
   def update_password(id, password_digest)
     User.find(id).update(password_digest: password_digest)
-      # accept an id and password_digest input parameters
-      # use the User Model class to update the `password_digest` for the User associated with the id primary key
-      # (no return is required)
   end
 
   def update_listname(id, name)
-      # accept an id and name input parameters
-      # use the TodoList Model class to update the `list_name` for the TodoList associated with id primary key 
-      # (no return is required)
+    TodoList.find(id).update(list_name: name)
   end 
 
   #
